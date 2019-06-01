d6e0b058f272fb5eb0028df6437872120a1ea084
Justin O'Halloran <justinohallo@gmail.com>
2019-05-31 17:42:56 -0700

    pre-eject Create React App

--- src/Routes.js
+++ src/Routes.js
@@ -1,15 +1,25 @@
 import React, { Component } from 'react'
 import {Route, Switch} from 'react-router-dom'
+// Components
+import Home from './Components/Home/Home'
+import Layout from './HOC/Layout/Layout'
+
 
 // Components 
 
 export class Routes extends Component {
     render() {
         return (
-
-           <Switch>
-               <Route path='/' exact compoent={Home}/>
-           </Switch>
+         
+            <Layout>
+                <Switch>
+               <Route path='/' exact component={Home}/>
+                </Switch>
+            </Layout>
+                
+                
+         
+         
         )
     }
 }
--- src/index.js
+++ src/index.js
@@ -2,12 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {BrowserRouter} from 'react-router-dom'
 
+
 import Routes from './Routes'
 
 const App = () => {
 return(
     <BrowserRouter>
         <Routes/>
+        
     </BrowserRouter>
 )
 }
